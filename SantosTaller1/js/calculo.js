f01dbaa12c93f86b01a5ecac489b849f906c5813
CristianSantos3C <100966065+CristianSantos3C@users.noreply.github.com>
2022-08-09 11:40:18 -0600

    Add files via upload

---
+++ SantosTaller1/js/calculo.js
@@ -0,0 +1,25 @@
+//variables 
+var cliente, masculino, femenino, edad, estudiante, total, monto=0, tiempo, meses;
+
+//Tasas
+const Tanual=0.15, tasaEdad=0.3, tasagenero= 0.5;
+
+//Datos
+
+var nombre= prompt ("Ingrese su nombre","");
+
+/*do{
+    estudiante = prompt("Usted es un estudiante? Digite si o no");
+    whitch(estudiante){
+        case 'SI': case 'si':
+
+
+
+
+    }
+
+}*/
+
+//Impresión
+document.write("<h1 bgcolor='black'>Su nombre es: "+nombre+" <h1/>");
+
